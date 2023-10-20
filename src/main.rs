8666e8123f2955981474e5f48bdf09a5c20eaa1d
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-10-20 21:39:35 +0000

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-use fruits::get_fruits;
-use fruits::add_fruit;
-use fruits::remove_fruit;
-use clap::Parser;
+@katelyn-hucker ➜ /workspaces/kh509_miniproject7 (main) $ cargo run
+   Compiling calc-cli-with-tests v0.1.0 (/workspaces/kh509_miniproject7)
+error[E0597]: `fruit` does not live long enough
+  --> src/main.rs:36:23
+   |
+35 |         Subcommand::Add { fruit } => {
+   |                           ----- binding `fruit` declared here
+36 |             add_fruit(&fruit);
+   |             ----------^^^^^^-
+   |             |         |
+   |             |         borrowed value does not live long enough
+   |             argument requires that `fruit` is borrowed for `'static`
+37 |             println!("Added fruit: {}", fruit);
+38 |         }
+   |         - `fruit` dropped here while still borrowed
 
-/// CLI tool to return random fruits
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// The quantity of fruits to return
-    #[clap(short, long, default_value = "1")]
-    count: u32,
-}
+error[E0597]: `fruit` does not live long enough
+  --> src/main.rs:40:26
+   |
+39 |         Subcommand::Remove { fruit } => {
+   |                              ----- binding `fruit` declared here
+40 |             remove_fruit(&fruit);
+   |             -------------^^^^^^-
+   |             |            |
+   |             |            borrowed value does not live long enough
+   |             argument requires that `fruit` is borrowed for `'static`
+41 |             println!("Removed fruit: {}", fruit);
+42 |         }
+   |         - `fruit` dropped here while still borrowed
 
-fn main() {
-    let args = Args::parse();
-    let fruits = get_fruits(args.count);
-    println!("fruits: {:?}", fruits);
-}
-
+For more information about this error, try `rustc --explain E0597`.
+error: could not compile `calc-cli-with-tests` (bin "calc-cli-with-tests") due to 2 previous errors
