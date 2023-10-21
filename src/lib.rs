05f0de927353189bc747f69495be4118a9b67329
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-10-21 21:50:48 +0000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,24 @@
 use std::sync::RwLock;
 
-
 lazy_static::lazy_static! {
-    static ref FRUITS: RwLock<Vec<&'static str>> = RwLock::new(Vec::new());
+    static ref FRUITS: RwLock<Vec<String>> = {
+        let fruits: Vec<String> = vec![
+            "banana".to_string(),
+            "apple".to_string(),
+        ];
+        RwLock::new(fruits)
+    };
 }
 
-
-pub fn add_fruit(fruit: &'static str) {
+pub fn add_fruit(fruit: &str) {
     let mut fruits = FRUITS.write().unwrap(); // Use write lock for exclusive access
-    fruits.push(fruit);
+    fruits.push(fruit.to_string());
 }
 
-pub fn remove_fruit(fruit: &'static str) {
+pub fn remove_fruit(fruit: &str) {
     let mut fruits = FRUITS.write().unwrap(); // Use write lock for exclusive access
-    fruits.retain(|&x| x != fruit);
+    fruits.retain(|x| x != fruit);
 }
-
 
 pub fn get_fruits() -> Vec<String> {
     let fruits = FRUITS.read().unwrap();
